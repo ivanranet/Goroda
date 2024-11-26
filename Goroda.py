9cd984bbc0657cc9f2d3956039318f29138bc287
ivanranet <ivanranet@yandex.ru>
2024-11-26 10:50:02 +0500

    добавил возвращение результата на русском языке

--- Goroda.py
+++ Goroda.py
@@ -1,11 +1,11 @@
+from auto_py_to_exe.config import language_hint
 from opencage.geocoder import OpenCageGeocode
 
 
 def get_coordinates(city, key):
     try:
         geocoder = OpenCageGeocode(key)
-        query = city
-        results = geocoder.geocode(query)
+        results = geocoder.geocode(city, language = 'ru')
         if results:
             return results[0]['geometry']['lat'], results[0]['geometry']['lng']
         else:
